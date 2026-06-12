dd68b48866cd844f1c7e22eb1d0f807280e964ee
saisanjana2410 <saisanjana.kambalapally.2410@gmail.com>
2026-06-12 11:40:37 -0400

    Initial development

---
+++ server.js
@@ -0,0 +1,13 @@
+import http from 'node:http'
+import { serveStatic } from './utils/serveStatic.js'
+
+const PORT = 8000
+
+const __dirname = import.meta.dirname
+
+const server = http.createServer(async (req,res) =>{
+
+    await serveStatic(req,res, __dirname)
+})
+
+server.listen(PORT, ()=> console.log(`connected on port: ${PORT}`))
